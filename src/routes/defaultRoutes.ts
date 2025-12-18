b6b493d04a26af1dd7926a2fd7e1acbc63dc34ef
mateiv-dev <mateiv.dev@gmail.com>
2025-12-18 19:26:08 +0200

    fix: remove /api from paths

--- src/routes/defaultRoutes.ts
+++ src/routes/defaultRoutes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (_req: Request, res: Response) => {
-    res.status(200).json({ message: "API Server is fully operational" });
+  res.status(200).json({ message: "API Server is fully operational" });
 });
 
 export default router;
